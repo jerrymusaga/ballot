9efcf5700c9c518b102d5baf87d0818abf2276e9
jerrymusaga <musagatechnology@gmail.com>
2023-03-01 01:04:11 +0100

    testing smart contracts

---
+++ test/Ballot.ts
@@ -0,0 +1,28 @@
+import { ethers } from "hardhat"
+import { Ballot } from "../typechain-types";
+
+const PROPOSALS = ["Proposal 1", "Proposal 2", "Proposal 3"];
+
+function convertStringArrayToByte32(array: string[]) {
+    let byteArray = [];
+    for(let i = 0; i < array.length; i++){  
+        byteArray.push(ethers.utils.formatBytes32String(array[i]));
+    }
+    return byteArray;
+}
+
+
+describe("Ballot", function (){
+
+    let ballotContract: Ballot;
+
+    beforeEach(async function (){
+        const ballotContractFactory = await ethers.getContractFactory("Ballot");
+        ballotContract = await ballotContractFactory.deploy(convertStringArrayToByte32(PROPOSALS));
+        await ballotContract.deployTransaction.wait();
+    })
+
+    it("has all the provided proposals", async function () {
+
+    })
+})

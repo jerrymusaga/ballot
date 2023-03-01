9ee2c06c9b8447b442ccb8e0ead469a1d6542b6e
jerrymusaga <musagatechnology@gmail.com>
2023-03-01 11:44:51 +0100

    testing smart contractss

--- test/Ballot.ts
+++ test/Ballot.ts
@@ -5,9 +5,10 @@ const PROPOSALS = ["Proposal 1", "Proposal 2", "Proposal 3"];
 
 function convertStringArrayToByte32(array: string[]) {
     let byteArray = [];
+
     for(let i = 0; i < array.length; i++){  
         byteArray.push(ethers.utils.formatBytes32String(array[i]));
-    }
+    } 
     return byteArray;
 }
 
@@ -20,9 +21,16 @@ describe("Ballot", function (){
         const ballotContractFactory = await ethers.getContractFactory("Ballot");
         ballotContract = await ballotContractFactory.deploy(convertStringArrayToByte32(PROPOSALS));
         await ballotContract.deployTransaction.wait();
+        console.log(`ballot deployed to address: ${ballotContract.address}` )
     })
 
     it("has all the provided proposals", async function () {
 
     })
+
+    it("assigns chairman to be the deployer of contract", async function(){
+
+    })
+
+
 })

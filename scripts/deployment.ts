883d78bd04a2df85fa262b63996c08e26a222660
jerrymusaga <musagatechnology@gmail.com>
2023-03-02 15:03:25 +0100

    deployment script

--- scripts/deployment.ts
+++ scripts/deployment.ts
@@ -1,8 +1,32 @@
 import { ethers } from "hardhat";
 
+const PROPOSALS = ["Proposal 1", "Proposal 2", "Proposal 3"];
+
+function convertStringArrayToByte32(array: string[]) {
+    let byteArray = [];
+
+    for(let i = 0; i < array.length; i++){  
+        byteArray.push(ethers.utils.formatBytes32String(array[i]));
+    } 
+    return byteArray;
+}
+
+
 async function main(){
     console.log("Deploying Ballot Contract");
     console.log("Proposals")
+
+    PROPOSALS.forEach((element, index) => {
+        console.log(`Proposal Index ${index}: ${element}`);
+    })
+
+    const ballotContractFactory = await ethers.getContractFactory("Ballot");
+    const ballotContract = await ballotContractFactory.deploy(convertStringArrayToByte32(PROPOSALS));
+    await ballotContract.deployTransaction.wait();
+    console.log(`ballot deployed to address: ${ballotContract.address}` )
+
+
+    
 }
 
 main().catch((error)=>{

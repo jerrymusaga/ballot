98013926554999ad40fa3f5383a4060544482109
jerrymusaga <musagatechnology@gmail.com>
2023-03-02 14:34:30 +0100

    deployment script

---
+++ scripts/deployment.ts
@@ -0,0 +1,11 @@
+import { ethers } from "hardhat";
+
+async function main(){
+    console.log("Deploying Ballot Contract");
+    console.log("Proposals")
+}
+
+main().catch((error)=>{
+    console.error(error);
+    process.exitCode = 1;
+})
